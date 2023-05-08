f35bfb37896c3b13fe3197372feb0c4fc34a02ee
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-05-08 16:46:27 +0100

    test

--- LaVittaEBella/Program.cs
+++ LaVittaEBella/Program.cs
@@ -1,12 +1,3 @@
-﻿using System;
-
-namespace LaVittaEBella
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9816844e7c6e272cc17fd1035b691553f6c6adae8aff0622740a308c90823a16
+size 196
