e1566bde3db42c87a09a7d76f2d7de878bfb5f29
Vitor <vitor.daniel@outlook.pt>
2023-06-01 11:46:23 +0100

    add plats

--- My project (1)/Assets/Scripts/camaux.cs
+++ My project (1)/Assets/Scripts/camaux.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:922a8db6a2d3b1abd2994c14236e482ae713746dfa97995907521d331cc591aa
-size 448
+oid sha256:88093ffd2b4d371290c40f967bb4c4e0a3b067cb176691c5c1e2c0e27c0efb0f
+size 564
