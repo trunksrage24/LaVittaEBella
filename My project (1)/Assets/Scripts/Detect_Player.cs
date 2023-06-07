cfd6b1bcd70d026ac2070bbaee10f6c40bcefd5a
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-06-07 19:02:56 +0100

    Add enemy follows player and returns to normal patrol path

---
+++ My project (1)/Assets/Scripts/Detect_Player.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4253b4ec41ffa20adff4be3773095bb3803de33e80834b76a2d518348e2b1309
+size 1368
--- My project (1)/Assets/Scripts/Enemy.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cc78cac665ec8a4ae340ba2081233f54ba6d0d1a141a56f2ce8c1c46dd9c0a9c
-size 307
