e78cc903df0847a55514c6b0fa9210702cb255ea
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-07-12 19:18:54 +0100

    test git add powerpoint

---
+++ LaVittaEBella/obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9203cf07d36b1aa135da4a5f86286f34432156cbaf6518c2909849eb7d577381
+size 190
---
+++ LaVittaEBella/obj/Debug/net6.0/LaVittaEBella.AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cdedae07b26c2e18c65cea90aa127016a806b9d299fa44607d2dec88e4c73e7f
+size 972
