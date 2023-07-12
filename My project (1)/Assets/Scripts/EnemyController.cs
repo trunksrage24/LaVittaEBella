792607f59a907bc887fa8faa13725c67ac912bec
Vitor <vitor.daniel@outlook.pt>
2023-07-12 16:58:02 +0100

    changed enemypatrol into enemycontroller and altered the code itself, created ground detector to help with the fisics of the game

---
+++ My project (1)/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:450d54179ff1e61f68bc1ae915b6cbad90907209824056d925f4f7fb549eeb15
+size 4388
--- My project (1)/Assets/Scripts/Enemy_Patrol.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ee2746da4c0d48d2e741826c676411cf8dad8b9e3c6a8d684441354159617db5
-size 2564
